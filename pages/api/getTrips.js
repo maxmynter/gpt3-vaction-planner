57eb6d0054acbd45cbd576a2d415c7ecda686412
Max Mynter <maxmynter@me.com>
2023-01-15 16:29:40 +0100

    Add mongo projection

--- pages/api/getTrips.js
+++ pages/api/getTrips.js
@@ -4,7 +4,11 @@ export default async (req, res) => {
   try {
     let { db } = await connectToDatabase();
 
-    const trips = await db.collection("trips").find({}).limit(100).toArray();
+    const trips = await db
+      .collection("trips")
+      .find({}, { _id: 1, text: 1, title: 1, prompts: 0 })
+      .limit(100)
+      .toArray();
 
     res.json(trips);
   } catch (e) {
