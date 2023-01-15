bb5230c51ae388314fa6aadb690a925a8d30702c
Max Mynter <maxmynter@me.com>
2023-01-15 23:07:57 +0100

    Style Header

--- pages/components/header.js
+++ pages/components/header.js
@@ -3,14 +3,22 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-link">
-        <Link href="/">Inspire</Link>
-      </div>
-      <div className="header-link">
-        <Link href="/popular">Other Trips</Link>
-      </div>
-      <div className="header-link">
-        <Link href="/how">How it works</Link>
+      <div className="header-link-container">
+        <div className="header-link">
+          <Link href="/" className="nextjs-header-link">
+            Inspire
+          </Link>
+        </div>
+        <div className="header-link">
+          <Link href="/popular" className="nextjs-header-link">
+            Other Trips
+          </Link>
+        </div>
+        <div className="header-link">
+          <Link href="/how" className="nextjs-header-link">
+            How it works
+          </Link>
+        </div>
       </div>
     </div>
   );
